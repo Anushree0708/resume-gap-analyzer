087a153eb6a88cea578c39192e0e860168ebd6eb
Anushree M <anushreem@Anushrees-MacBook-Air.local>
2026-03-13 11:46:51 +0530

    fix dependencies

--- backend/utils.py
+++ backend/utils.py
@@ -1,18 +1,16 @@
-import pdfplumber
+from PyPDF2 import PdfReader
 import io
 
-
 def extract_text_from_pdf(pdf_bytes):
+
+    pdf_stream = io.BytesIO(pdf_bytes)
+    reader = PdfReader(pdf_stream)
+
     text = ""
 
-    try:
-        with pdfplumber.open(io.BytesIO(pdf_bytes)) as pdf:
-            for page in pdf.pages:
-                page_text = page.extract_text()
-                if page_text:
-                    text += page_text + "\n"
-
-    except Exception as e:
-        raise Exception(f"Error extracting text from PDF: {str(e)}")
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
 
     return text
